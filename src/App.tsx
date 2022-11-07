49d78b127e6ee48c7dcb45adb1d39f5b831bc00b
mohiandipta <mohianmostofa@gmail.com>
2022-11-07 16:00:33 +0600

    hangman structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+import words from "./wordList.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [wordToGuess, setwordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{
+      maxWidth:"800px",
+      display:"flex",
+      flexDirection:"column",
+      gap:"2rem",
+      margin:"0 auto",
+      alignItems:"center"
+    }}>
+      <div style={{fontSize:"2rem", textAlign:"center"}}>
+        Lose Win
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
     </div>
   )
 }
